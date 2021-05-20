262a548a540e959586e36c6a0078aba3b247a5be
Rasse666 <77397377+Rasse666@users.noreply.github.com>
2021-05-20 13:47:37 +0300

    -

---
+++ js/helloword.js
@@ -0,0 +1,9 @@
+window.onload = function() 
+{
+    console.log("Tämä kirjoitetaan selaimen konsoliin");
+}
+function sayHelloTo(name) {
+    alert("Hei " + name + "!");
+    console.log("sayHello-funktio suoritettu");
+    }
+    
